279e460003b5cadf2dc4be0c7d5d13ad1312c20c
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-01-07 08:05:47 +0200

    added to filter answered from unanswered

--- components/Questions/Questions.tsx
+++ components/Questions/Questions.tsx
@@ -1,23 +1,38 @@
 import styles from "./styles.module.css";
 import QuestionCard from "../QuestionCard/QuestionCard";
 import { QuestionType } from "@/types";
+import { useState } from "react";
 type QuestionPropsType = {
   questions: QuestionType[];
 };
 const Questions = ({ questions }: QuestionPropsType) => {
+  const [sortVal, setSortVal] = useState("All");
+
   return (
     <div className={styles.main}>
-      {questions.map((q) => (
-        <QuestionCard
-          key={q.id}
-          date={q.date}
-          id={q.id}
-          title={q.title}
-          name={q.name}
-          question={q.question_text}
-          answerNumber={q.answers}
-        />
-      ))}
+      <select onChange={(e) => setSortVal(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Answered">Answered</option>
+        <option value="Unanswered">Unanswered</option>
+      </select>
+      {questions
+        .filter(
+          (a) =>
+            (sortVal == "All" && a.answers >= 0) ||
+            (sortVal == "Answered" && a.answers !== 0) ||
+            (sortVal == "Unanswered" && a.answers == 0)
+        )
+        .map((q) => (
+          <QuestionCard
+            key={q.id}
+            date={q.date}
+            id={q.id}
+            title={q.title}
+            name={q.name}
+            question={q.question_text}
+            answerNumber={q.answers}
+          />
+        ))}
     </div>
   );
 };
