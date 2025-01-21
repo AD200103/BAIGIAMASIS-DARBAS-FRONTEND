6068a9e9298467ca7980c9265b596ef816c21bbe
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-01-21 16:53:58 +0200

    2nd try

--- utils/likesDislikesUpd.ts
+++ utils/likesDislikesUpd.ts
@@ -1,21 +1,24 @@
+type BodyType = {
+  pressed: string;
+};
 const updateLikesDislikes = (
-  body,
-  userIdFromToken,
-  setUserDislikeIdArr,
-  setUserLikeIdArr,
-  setDislikesAmmount,
-  setLikesAmmount,
-  userLikeIdArr,
-  userDislikeIdArr
+  body: BodyType,
+  userIdFromToken: string | undefined,
+  setUserDislikeIdArr: React.Dispatch<React.SetStateAction<string[]>>,
+  setUserLikeIdArr: React.Dispatch<React.SetStateAction<string[]>>,
+  setDislikesAmmount: React.Dispatch<React.SetStateAction<number>>,
+  setLikesAmmount: React.Dispatch<React.SetStateAction<number>>,
+  userLikeIdArr: string[],
+  userDislikeIdArr: string[]
 ) => {
   if (body.pressed == "like pressed") {
-    if (userLikeIdArr.includes(userIdFromToken)) {
+    if (userLikeIdArr.includes(userIdFromToken!)) {
       setUserLikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
     } else {
-      setUserLikeIdArr((prev) => [...prev, userIdFromToken]);
+      setUserLikeIdArr((prev) => [...prev, userIdFromToken!]);
     }
     setLikesAmmount(userLikeIdArr.length);
-    if (userDislikeIdArr.includes(userIdFromToken)) {
+    if (userDislikeIdArr.includes(userIdFromToken!)) {
       setUserDislikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
     } else {
       setUserDislikeIdArr(userDislikeIdArr);
@@ -24,13 +27,13 @@ const updateLikesDislikes = (
     return;
   }
   if (body.pressed == "dislike pressed") {
-    if (userDislikeIdArr.includes(userIdFromToken)) {
+    if (userDislikeIdArr.includes(userIdFromToken!)) {
       setUserDislikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
     } else {
-      setUserDislikeIdArr((prev) => [...prev, userIdFromToken]);
+      setUserDislikeIdArr((prev) => [...prev, userIdFromToken!]);
     }
     setDislikesAmmount(userDislikeIdArr.length);
-    if (userLikeIdArr.includes(userIdFromToken)) {
+    if (userLikeIdArr.includes(userIdFromToken!)) {
       setUserLikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
     } else {
       setUserLikeIdArr(userLikeIdArr);
