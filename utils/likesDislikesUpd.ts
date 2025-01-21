71f10f53852b4901df59b570cc984c8e9d720abb
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-01-21 17:02:41 +0200

    3rd try

--- utils/likesDislikesUpd.ts
+++ utils/likesDislikesUpd.ts
@@ -14,70 +14,40 @@ const updateLikesDislikes = (
   if (body.pressed == "like pressed") {
     if (userLikeIdArr.includes(userIdFromToken!)) {
       setUserLikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
-    } else {
-      setUserLikeIdArr((prev) => [...prev, userIdFromToken!]);
+      setLikesAmmount(userLikeIdArr.length);
     }
-    setLikesAmmount(userLikeIdArr.length);
+    if (!userLikeIdArr.includes(userIdFromToken!)) {
+      setUserLikeIdArr((prev) => [...prev, userIdFromToken!]);
+      setLikesAmmount(userLikeIdArr.length);
+    }
     if (userDislikeIdArr.includes(userIdFromToken!)) {
       setUserDislikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
-    } else {
-      setUserDislikeIdArr(userDislikeIdArr);
+      setDislikesAmmount(userDislikeIdArr.length);
     }
-    setDislikesAmmount(userDislikeIdArr.length);
+    if (!userDislikeIdArr.includes(userIdFromToken!)) {
+      setUserDislikeIdArr(userDislikeIdArr);
+      setDislikesAmmount(userDislikeIdArr.length);
+    }
     return;
   }
   if (body.pressed == "dislike pressed") {
     if (userDislikeIdArr.includes(userIdFromToken!)) {
       setUserDislikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
-    } else {
-      setUserDislikeIdArr((prev) => [...prev, userIdFromToken!]);
+      setDislikesAmmount(userDislikeIdArr.length);
     }
-    setDislikesAmmount(userDislikeIdArr.length);
+    if (!userDislikeIdArr.includes(userIdFromToken!)) {
+      setUserDislikeIdArr((prev) => [...prev, userIdFromToken!]);
+      setDislikesAmmount(userDislikeIdArr.length);
+    }
     if (userLikeIdArr.includes(userIdFromToken!)) {
       setUserLikeIdArr((prev) => prev.filter((p) => p !== userIdFromToken));
-    } else {
-      setUserLikeIdArr(userLikeIdArr);
+      setLikesAmmount(userLikeIdArr.length);
     }
-    setLikesAmmount(userLikeIdArr.length);
+    if (!userLikeIdArr.includes(userIdFromToken!)) {
+      setUserLikeIdArr(userLikeIdArr);
+      setLikesAmmount(userLikeIdArr.length);
+    }
     return;
   }
 };
 export default updateLikesDislikes;
-
-// const updateLikeDislike = (req, findAnswerToUpdate) => {
-//     if (req.body.pressed == "dislike pressed") {
-//       const body = {
-//         usersWhoDislikedTheAnswer:
-//           findAnswerToUpdate.usersWhoDislikedTheAnswer.includes(req.body.userId)
-//             ? findAnswerToUpdate.usersWhoDislikedTheAnswer.filter(
-//                 (id) => id !== req.body.userId
-//               )
-//             : [...findAnswerToUpdate.usersWhoDislikedTheAnswer, req.body.userId],
-//         usersWhoLikedTheAnswer:
-//           findAnswerToUpdate.usersWhoLikedTheAnswer.includes(req.body.userId)
-//             ? findAnswerToUpdate.usersWhoLikedTheAnswer.filter(
-//                 (id) => id !== req.body.userId
-//               )
-//             : findAnswerToUpdate.usersWhoLikedTheAnswer,
-//       };
-//       return body;
-//     }
-//     if (req.body.pressed == "like pressed") {
-//       const body = {
-//         usersWhoLikedTheAnswer:
-//           findAnswerToUpdate.usersWhoLikedTheAnswer.includes(req.body.userId)
-//             ? findAnswerToUpdate.usersWhoLikedTheAnswer.filter(
-//                 (id) => id !== req.body.userId
-//               )
-//             : [...findAnswerToUpdate.usersWhoLikedTheAnswer, req.body.userId],
-//         usersWhoDislikedTheAnswer:
-//           findAnswerToUpdate.usersWhoDislikedTheAnswer.includes(req.body.userId)
-//             ? findAnswerToUpdate.usersWhoDislikedTheAnswer.filter(
-//                 (id) => id !== req.body.userId
-//               )
-//             : findAnswerToUpdate.usersWhoDislikedTheAnswer,
-//       };
-//       return body;
-//     }
-//   };
-//   export default updateLikeDislike;
