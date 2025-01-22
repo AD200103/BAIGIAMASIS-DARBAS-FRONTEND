15afcc18037f7b473d9afe047346051f67f8db9b
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-01-22 17:17:31 +0200

    date try 3

--- utils/dateAndEmail.ts
+++ utils/dateAndEmail.ts
@@ -1,17 +1,27 @@
 import "dotenv/config";
-type DateOptionType = {
-  hour: "2-digit" | "numeric" | undefined;
-  minute: "2-digit" | "numeric" | undefined;
-  hour12?: boolean;
-  timeZone?: string;
+type ObjectDateType = {
+  year: "numeric" | "2-digit" | undefined;
+  month: "numeric" | "2-digit" | undefined;
+  day: "numeric" | "2-digit" | undefined;
+  hour: "numeric" | "2-digit" | undefined;
+  minute: "numeric" | "2-digit" | undefined;
+  second: "numeric" | "2-digit" | undefined;
+  hour12: boolean;
+  timeZone: string;
 };
 export const dateConvert = (date: Date) => {
-  const options: DateOptionType = {
+  const options: ObjectDateType = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
+    second: "2-digit",
     hour12: false,
     timeZone: "Europe/Vilnius",
-  } as DateOptionType;
-  const formattedTime = new Intl.DateTimeFormat("lt-LT", options).format(date);
-  return formattedTime;
+  };
+  const lithuanianDateTime = new Intl.DateTimeFormat("lt-LT", options).format(
+    date
+  );
+  return lithuanianDateTime;
 };
